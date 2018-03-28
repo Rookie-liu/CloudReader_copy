5ee4f39dce02badb92aeefd779ca119b7f91d80b
liuyafang <liuyafang@tton.co>
2018-03-28 17:24:07 +0800

    启动页 抽屉

--- app/src/main/java/com/example/rookie/cloudreader_c/MainActivity.java
+++ app/src/main/java/com/example/rookie/cloudreader_c/MainActivity.java
@@ -1,18 +1,305 @@
 package com.example.rookie.cloudreader_c;
 
+import android.app.ActionBar;
+import android.app.Fragment;
 import android.databinding.DataBindingUtil;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
+import com.example.rookie.cloudreader_c.app.ConstantsImageUrl;
+import com.example.rookie.cloudreader_c.databinding.ActivityBaseBinding;
 import com.example.rookie.cloudreader_c.databinding.ActivityMainBinding;
+import com.example.rookie.cloudreader_c.databinding.NavHeaderMainBinding;
+import com.example.rookie.cloudreader_c.http.rx.RxBus;
+import com.example.rookie.cloudreader_c.http.rx.RxBusBaseMessage;
+import com.example.rookie.cloudreader_c.http.rx.RxCodeConstants;
+import com.example.rookie.cloudreader_c.utils.CommonUtils;
+import com.example.rookie.cloudreader_c.utils.ImgLoadUtil;
+import com.example.rookie.cloudreader_c.utils.PerfectClickListener;
+import com.example.rookie.cloudreader_c.utils.SPUtils;
+import com.example.rookie.cloudreader_c.view.MyFragmentPagerAdapter;
+import com.example.rookie.cloudreader_c.view.statusbar.StatusBarUtil;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+import rx.Subscriber;
+import rx.Subscription;
+import rx.subscriptions.CompositeSubscription;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, ViewPager.OnPageChangeListener {
+
+    private FrameLayout llTitleMenu;
+    private Toolbar toolbar;
+    private FloatingActionButton fab;
+    private NavigationView navView;
+    private DrawerLayout drawerLayout;
+    private ViewPager vpContent;
+
+    // 对应的bean
     private ActivityMainBinding mBinding;
+    private ImageView llTitleGank;
+    private ImageView llTitleOne;
+    private ImageView llTitleDou;
+    private CompositeSubscription mCompositeSubscription;
+    private NavHeaderMainBinding bind;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        initStatusView();
+        initId();
+        initRxBus();
+
+        //TODO 这里getColor 报错...是不是utils 类些写的有错
+//        StatusBarUtil.setColorNoTranslucentForDrawerLayout(MainActivity.this, drawerLayout, CommonUtils.getColor(R.color.colorTheme));
+        StatusBarUtil.setColorNoTranslucentForDrawerLayout(MainActivity.this, drawerLayout, 123);
+        initContentFragment();
+        initDrawerLayout();
+        initListener();
+
+    }
+
+    // 初始化view
+    private void initStatusView() {
+        ViewGroup.LayoutParams layoutParams = mBinding.include.viewStatus.getLayoutParams();
+        layoutParams.height = StatusBarUtil.getStatusBarHeight(this);
+        mBinding.include.viewStatus.setLayoutParams(layoutParams);
+    }
+
+    //初始化ID  TODO 但是为什么又的需要icnloud ? 是因为写的插件的原因艾玛
+    private void initId() {
+        drawerLayout = mBinding.drawerLayout;
+        navView = mBinding.navView;
+        fab = mBinding.include.fab;
+        toolbar = mBinding.include.toolbar;
+        llTitleMenu = mBinding.include.llTitleMenu;
+        vpContent = mBinding.include.vpContent;
+        fab.setVisibility(View.GONE);  // TODO 这里是吧什么隐藏了呢
+
+        llTitleGank = mBinding.include.ivTitleGank;
+        llTitleOne = mBinding.include.ivTitleOne;
+        llTitleDou = mBinding.include.ivTitleDou;
+    }
+
+    // 初始化抽屉的一些内容
+    private void initDrawerLayout() {
+        navView.inflateHeaderView(R.layout.nav_header_main);
+        View headerView = navView.getHeaderView(0);
+        bind = DataBindingUtil.bind(headerView);
+        bind.setListener(this);
+        //TODO error 报错. 注释掉
+        //        bind.dayNightSwitch.setChecked(SPUtils.getNightMode());
+
+        ImgLoadUtil.displayCircle(bind.ivAvatar, ConstantsImageUrl.IC_AVATAR);
+        bind.llNavExit.setOnClickListener(this);
+        bind.ivAvatar.setOnClickListener(this);
+
+        bind.llNavHomepage.setOnClickListener(this);
+        bind.llNavScanDownload.setOnClickListener(this);
+        bind.llNavDeedback.setOnClickListener(this);
+        bind.llNavAbout.setOnClickListener(this);
+        bind.llNavLogin.setOnClickListener(this);
+    }
+
+    // 绑定点击方法
+    private void initListener() {
+        llTitleMenu.setOnClickListener(this);
+        mBinding.include.ivTitleGank.setOnClickListener(this);
+        mBinding.include.ivTitleDou.setOnClickListener(this);
+        mBinding.include.ivTitleOne.setOnClickListener(this);
+        fab.setOnClickListener(this);
+    }
+
+    // 初始化加载三个页面. 放到一个 adapter里面
+    private void initContentFragment() {
+        ArrayList<Fragment> mFragmentList = new ArrayList<>();
+//        mFragmentList.add(new OneFragment());
+//        mFragmentList.add(new GankFragment());
+//        mFragmentList.add(new BookFragment());
+        MyFragmentPagerAdapter adapter = new MyFragmentPagerAdapter(getSupportFragmentManager(), mFragmentList);
+        vpContent.setAdapter(adapter);
+        vpContent.setOffscreenPageLimit(2);
+        vpContent.addOnPageChangeListener(this);
+        mBinding.include.ivTitleGank.setSelected(true);
+        vpContent.setCurrentItem(0);
+
+        setSupportActionBar(toolbar);
+        // TODO 这里解开注释提示不兼容类型
+//        ActionBar actionBar = getSupportActionBar();
+//        if (actionBar != null) {
+//            actionBar.setDisplayShowTitleEnabled(false);
+//        }
+        llTitleGank.setSelected(true);
+        llTitleDou.setSelected(true);
+        llTitleOne.setSelected(true);
+        vpContent.setCurrentItem(1);
+    }
+
+    // 抽屉里面的点击方法
+    private PerfectClickListener listener = (new PerfectClickListener() {
+        @Override
+        protected void onNoDoubleClick(View v) {
+            mBinding.drawerLayout.closeDrawer(GravityCompat.START);
+            mBinding.drawerLayout.postDelayed(() -> {
+                switch (v.getId()) {
+                    case R.id.ll_nav_homepage:
+
+                        break;
+                    case R.id.ll_nav_scan_download:
+                        break;
+                    case R.id.ll_nav_deedback:
+                        break;
+                    case R.id.ll_nav_about:
+                        break;
+                    case R.id.ll_nav_login:
+                        break;
+                    default:
+                        break;
+                }
+
+
+            }, 260);
+        }
+    });
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.ll_title_menu: // 开启菜单
+                drawerLayout.openDrawer(GravityCompat.START);
+                break;
+            case R.id.iv_title_gank: //干货
+                break;
+            case R.id.iv_title_one: // 电影
+                break;
+            case R.id.iv_title_dou: // 书籍
+                break;
+            case R.id.iv_avatar: // 进入到github
+                break;
+            case R.id.ll_nav_exit:
+                finish();
+                break;
+            default:
+                break;
+
+        }
+    }
+
+    //夜间模式 , 暂时还没有
+    public boolean getNightMode() {
+        return SPUtils.getNightMode();
+    }
+    public void onNightModeClick(View view) {
+        if (!SPUtils.getNightMode()) {
+//            SkinCompatManager.getInstance().loadSkin(Constants.NIGHT_SKIN);
+        } else {
+            // 恢复应用默认皮肤
+//            SkinCompatManager.getInstance().restoreDefaultTheme();
+        }
+        SPUtils.setNightMode(!SPUtils.getNightMode());
+        bind.dayNightSwitch.setChecked(SPUtils.getNightMode());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case  R.id.action_search:
+                Toast.makeText(this, "搜索", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        switch (position) {
+            case 0:
+                llTitleGank.setSelected(false);
+                llTitleOne.setSelected(true);
+                llTitleDou.setSelected(false);
+                break;
+            case 1:
+                llTitleOne.setSelected(false);
+                llTitleGank.setSelected(true);
+                llTitleDou.setSelected(false);
+                break;
+            case 2:
+                llTitleDou.setSelected(true);
+                llTitleOne.setSelected(false);
+                llTitleGank.setSelected(false);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else  {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            if (mBinding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                mBinding.drawerLayout.closeDrawer(GravityCompat.START);
+            }else  {
+                moveTaskToBack(true);
+            }
+            return true;
+        }
+        return  super.onKeyDown(keyCode, event);
+    }
+
+    private void initRxBus() {
+        Subscription subscribe = RxBus.getDefault().toObservable(RxCodeConstants.JUMP_TYPE_TO_ONE, RxBusBaseMessage.class)
+                .subscribe(integer -> mBinding.include.vpContent.setCurrentItem(0));
+        addSubscription(subscribe);
+
+    }
+    public void addSubscription(Subscription s) {
+        if (this.mCompositeSubscription == null) {
+            this.mCompositeSubscription = new CompositeSubscription();
+        }
+        this.mCompositeSubscription.add(s);
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (this.mCompositeSubscription != null && mCompositeSubscription.hasSubscriptions()) {
+            this.mCompositeSubscription.unsubscribe();
+        }
     }
 }
--- app/src/main/java/com/example/rookie/cloudreader_c/ui/TransitionActivity.java
+++ app/src/main/java/com/example/rookie/cloudreader_c/ui/TransitionActivity.java
@@ -2,12 +2,19 @@ package com.example.rookie.cloudreader_c.ui;
 
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
 
+import com.bumptech.glide.Glide;
+import com.example.rookie.cloudreader_c.MainActivity;
 import com.example.rookie.cloudreader_c.R;
 import com.example.rookie.cloudreader_c.app.ConstantsImageUrl;
 import com.example.rookie.cloudreader_c.databinding.ActivityTransitionBinding;
+import com.example.rookie.cloudreader_c.utils.CommonUtils;
+import com.example.rookie.cloudreader_c.utils.PerfectClickListener;
 
 import java.util.Random;
 
@@ -29,7 +36,83 @@ public class TransitionActivity extends AppCompatActivity {
             return;
         }
 
+        // 随机获取一个启动图地址
         int i= new Random().nextInt(ConstantsImageUrl.TRANSITION_URLS.length);
-//        mBinding.ivDefultPic.setImageDrawable(common);
+        // 获取启动图, 先显示默认启动图
+
+        //TODO error 这里报错
+//        mBinding.ivDefultPic.setImageDrawable(CommonUtils.getDrawable(R.drawable.img_transition_default));
+
+        //TODO 这个方法是进行图片请求的嘛?Glide 常用的图片加载的三方
+        Glide.with(this)
+                .load(ConstantsImageUrl.TRANSITION_URLS[i])
+                .placeholder(R.drawable.img_transition_default)
+                .error(R.drawable.img_transition_default)
+                .into(mBinding.ivPic);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 延迟1500 隐藏 启动图?
+                mBinding.ivDefultPic.setVisibility(View.GONE);
+            }
+        }, 1500);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // 延迟3500 进入主页
+                toMainActivity();
+            }
+        }, 3500);
+
+        // 点击跳过的方法
+        mBinding.tvJump.setOnClickListener(new PerfectClickListener() {
+            @Override
+            protected void onNoDoubleClick(View v) {
+                toMainActivity();
+            }
+        });
+
+    }
+
+    // 鉴定效果跳转到主页
+    private Animation.AnimationListener animationListener = new Animation.AnimationListener() {
+        @Override
+        public void onAnimationStart(Animation animation) {
+            animationEnd();
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+    };
+
+    private void animationEnd() {
+        synchronized (TransitionActivity.this) {
+            if (!animationEnd) {
+                animationEnd = true;
+                mBinding.ivPic.clearAnimation();
+                toMainActivity();
+            }
+        }
+    }
+
+    private void toMainActivity() {
+        if (isIn){
+            return;
+        }
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.screen_zoom_in, R.anim.screen_zoom_out);
+        finish();
+        isIn = true;
+
     }
 }
